f2cc9f117bc8453c5b6d360b2f0e0b5a768f2943
PilotPrix <69162332+PilotPrix@users.noreply.github.com>
2022-05-21 10:13:44 -0400

    Add procedure getMaxDistance()
    Testing required, add features:
    - Draw line representing distance
    - Display text beside it

--- main.py
+++ main.py
@@ -1,19 +1,67 @@
 import cv2
+from cv2 import sqrt
 import pose_detection_module as pdm
 import time
+import threading
 
+# Initialization code
 cap = cv2.VideoCapture(0)
 pTime = 0
 detector = pdm.poseDetector()
 
+userInput = ""
+maxDistance = 0
+
+# Initialize procedures
+def userInputProcedure(x):
+    global userInput
+    userInput = input(x)
+
+def getMaxDistance():
+    # In order to run computer vision detection system and ask user input simultaneously, start a thread
+    userInputThread = threading.Thread(target=userInputProcedure, args="type \"y\" in the console to confirm max distance between nose and upper chest")
+    userInputThread.start()
+
+    # Computer vision detection system
+    while userInput == "":
+        # Read image
+        success, img = cap.read()
+        img = detector.findPose(img, True)
+        lmList = detector.findPosition(img)
+
+        # Get landmarks
+        x1, y1 = lmList[11]
+        x2, y2 = lmList[12]
+
+        # Get upper chest by averaging landmark 11 and 12
+        upperChest = [(x1 + x2) / 2, (y1 + y2) / 2]
+        nose = lmList[0]
+
+        distance = sqrt(upperChest ** 2 + nose ** 2)
+
+        # Display distance on screen
+        cv2.putText(img, distance, (70, 100), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+
+        # Show image
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+    
+    # Store the distance to maxDistance
+    global maxDistance
+    maxDistance = distance
+
+
+# Introduction
+print("Hello and welcome to $NAMEOFOURSOFTWARE!")
+getMaxDistance()
+
+# This piece of code is just for reference for now
 while True:
     # Read image
     success, img = cap.read()
     img = detector.findPose(img, True)
     lmList = detector.findPosition(img)
-    if lmList:
-        print(lmList[14])
-
+    
     # Calculating FPS
     cTime = time.time()
     fps = 1/(cTime-pTime)
