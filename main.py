8e39fb2a97cdf63ab6d4eeed031e0d278ac023f8
Anika Sharma <sharmaanika2005@gmail.com>
2022-05-21 16:23:15 -0400

    Co-authored-by: acrenw <acrenw@users.noreply.github.com> Co-authored-by: Sean Wang <seanwaterloo2997@gmail.com> Co-authored-by: sarahkwak79 <sarahkwak79@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,60 +3,95 @@ from math import sqrt
 import Pose_detection_module as pdm
 import time
 import threading
+import tkinter
 
 # Initialization code
 cap = cv2.VideoCapture(0)
 pTime = 0
 detector = pdm.poseDetector()
 
-userInput = ""
-maxDistance = 0
+# userInput = ""
+
+# The list that stores the max distance (good posture) and min distance (bad posture)
+Dist = [] 
 
 # Initialize procedures
 def userInputProcedure(x):
     global userInput
     userInput = input(x)
 
-def getMaxDistance():
+def getDistance():
+
+    # THREADING STUFF
     # In order to run computer vision detection system and ask user input simultaneously, start a thread
-    userInputThread = threading.Thread(target=userInputProcedure, args="type \"y\" in the console to confirm max distance between nose and upper chest")
-    userInputThread.start()
+    # userInputThread = threading.Thread(target=userInputProcedure, args="type \"y\" in the console to confirm max distance between nose and upper chest")
+    # userInputThread.start()
 
     # Computer vision detection system
-    while userInput == "":
-        # Read image
-        success, img = cap.read()
-        img = detector.findPose(img, True)
-        lmList = detector.findPosition(img)
-
-        # Get landmarks
-        x1, y1 = lmList[11] # left shoulder
-        x2, y2 = lmList[12] # right shoulder
-
-        # Get upper chest by averaging landmark 11 and 12
-        upperChest = [(x1 + x2) / 2, (y1 + y2) / 2]
-        nose = lmList[0]
-
-        # Distance between the nose and chest
-        distance = sqrt(abs(upperChest[0] - nose[0]) ** 2 + abs(upperChest[1] - nose[1]) ** 2)
-
-        # Display distance on screen
-        cv2.putText(img, distance, (70, 100), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
-
-        # Show image
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
     
-    # Store the distance to maxDistance
-    global maxDist
-    maxDist = distance
+    # Read image
+    global img
+
+    success, img = cap.read()
+    img = detector.findPose(img, True)
+    lmList = detector.findPosition(img)
+
+    # Get landmarks
+
+    x1, y1 = lmList[11] # left shoulder
+    x2, y2 = lmList[12] # right shoulder
+
+    # Get upper chest by averaging landmark 11 and 12
+    upperChest = [(x1 + x2) / 2, (y1 + y2) / 2]
+    nose = lmList[0]
+
+    # Distance between the nose and chest
+    distance = sqrt(abs(upperChest[0] - nose[0]) ** 2 + abs(upperChest[1] - nose[1]) ** 2)
+    
+    # Store the distance to our list 
+    Dist.append(distance)
+
+    # Show image
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+    
+    
+
+def slouchAlert():
+    
+    pass
 
 
-# Introduction
+def distancePerFrame():
+    pass
+
+
+
+#_________________________________________________________________________#
+
+# Introduction and Data Gathering
 print("Hello and welcome to $NAMEOFOURSOFTWARE!")
-getMaxDistance()
-print(maxDist)
 
+getDistance()
+print("----------------------- this is the max distance", Dist[0])
+
+cv2.putText(img, "go slouch", (50,50), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 5)
+cv2.imshow("Image", img)
+cv2.waitKey(1)
+
+time.sleep(5)
+
+getDistance()
+print("----------------------- this is the min distance", Dist[1])
+
+#Average of max and min distance 
+Threshold = (Dist[0] + Dist[1])/2
+
+
+
+
+#_________________________________________________________________________#
 # This piece of code is just for reference for now
 # while True:
 #     # Read image
